64e52995146e4b9cee0132a976ef314a16d3339a
nathanjzhao <43712744+nathanjzhao@users.noreply.github.com>
2026-03-27 04:22:08 -0400

    release: update to v0.4.3

--- Casks/taiku.rb
+++ Casks/taiku.rb
@@ -3,10 +3,10 @@ cask "taiku" do
 
   if Hardware::CPU.arm?
     url "https://taiku-releases.s3.amazonaws.com/taiku-desktop-aarch64-apple-darwin.tar.gz"
-    sha256 "78b9c0e39150081afde672f6f123ac5fef65d4a22e351596cf419d0c0183ecce"
+    sha256 "689d3b61c2e8e6b9b1125832087d9037d3b5c6624291b453ffa506a050a654c4"
   else
     url "https://taiku-releases.s3.amazonaws.com/taiku-desktop-x86_64-apple-darwin.tar.gz"
-    sha256 "e51d5171fd188aece065dc1ac8d1455118a7df4abad82057002da7281629fec8"
+    sha256 "796cb09090d567a0fdd16dbffe645588d9741a55725486c0fdcfa14efb7c3600"
   end
 
   name "taiku"
--- Formula/taiku.rb
+++ Formula/taiku.rb
@@ -7,20 +7,20 @@ class Taiku < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://taiku-releases.s3.amazonaws.com/taiku-aarch64-apple-darwin.tar.gz"
-      sha256 ""
+      sha256 "94da58f66a54688385e7e9c31105e108dde15363b911d6ac54f69e8d354ff3e0"
     else
       url "https://taiku-releases.s3.amazonaws.com/taiku-x86_64-apple-darwin.tar.gz"
-      sha256 ""
+      sha256 "70085a8d3a6382686829ee4683bd7830bca7b63ed788a2e788444616b35ad6de"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://taiku-releases.s3.amazonaws.com/taiku-aarch64-unknown-linux-musl.tar.gz"
-      sha256 ""
+      sha256 "40ee243fc0f73429096a43fed93457c57795273c90d61bd90b6260ca5751a958"
     else
       url "https://taiku-releases.s3.amazonaws.com/taiku-x86_64-unknown-linux-musl.tar.gz"
-      sha256 ""
+      sha256 "151f8487d04185ad20e1c1863c5139d9fa620cc1c354af0d2825b0255f92bd0f"
     end
   end
 
